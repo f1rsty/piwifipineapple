c32f8b98f965ab6efd6c2f99359f2013c00417fb
Jason Sholler <36761536+f1rsty@users.noreply.github.com>
2019-01-12 09:23:07 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<h1>Please login to continue</h1>
+<body>
+	<form action="submit.php" method="post">
+		Email Address:<br>
+		<input type="text" name="uname">
+			<br>
+		Password:<br>
+		<input type="password" name="password">
+			<br><br>
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
