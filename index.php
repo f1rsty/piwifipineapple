fc5b180df16b047ff2aedda60ead387fd3cfec56
Jason Sholler <36761536+f1rsty@users.noreply.github.com>
2019-07-02 08:25:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
-<html>
-<h1>Please login to continue</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="style.css">
+	<title>Login Form</title>
+</head>
+
 <body>
-	<form action="submit.php" method="post">
-		Email Address:<br>
-		<input type="text" name="uname">
-			<br>
-		Password:<br>
-		<input type="password" name="password">
-			<br><br>
-		<input type="submit" value="Submit">
+	<h1>Please login to continue</h1>
+	<form action="submit.php">
+	<div class="container">
+		<label for="uname"><b>Username</b></label>
+		<input type="text" placeholder="Enter Username" name="uname" required>
+	
+		<label for="psw"><b>Password</b></label>
+		<input type="password" placeholder="Enter Password" name="psw" required>
+			
+		<button type="submit">Login</button>
+	  </div>
 	</form>
 </body>
+
 </html>
